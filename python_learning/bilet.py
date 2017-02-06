38451c1d699704f8e3a75dbb8dfa8940004cf3ad
Oleksandr Sebestianovych <oseb@softserveinc.com>
2017-02-06 14:23:13 +0200

    happy ticket update

--- python_learning/bilet.py
+++ python_learning/bilet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Проверка на счастливый билет"""
-a = str(input())
+a = raw_input()
 b = int(a[0]) + int(a[1]) + int(a[2])
 c = int(a[3]) + int(a[4]) + int(a[5])
 if b == c:
