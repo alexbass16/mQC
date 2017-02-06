496cfb223d6f7b1f1973417c4c41a3721b1a1069
Oleksandr Sebestianovych <oseb@softserveinc.com>
2017-02-06 14:19:00 +0200

    happy ticket

---
+++ python_learning/bilet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Проверка на счастливый билет"""
+a = str(input())
+b = int(a[0]) + int(a[1]) + int(a[2])
+c = int(a[3]) + int(a[4]) + int(a[5])
+if b == c:
+    print("Счастливый")
+else:
+    print("Обычный")
